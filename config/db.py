a020bd038f2dda64973998b477dccbe3077d354d
sckhoo <sckhoo@gmail.com>
2024-02-27 14:24:56 +0800

    update db connection and collection

--- config/db.py
+++ config/db.py
@@ -7,10 +7,12 @@ load_dotenv()
 MDB = environ.get("MDB")
 MDB_USER = environ.get("MDB_USER")
 MDB_PW = environ.get("MDB_PW")
+MDB_DB_INSTANCE = environ.get("MDB_DB_INSTANCE")
+MDB_DB_COLLECTION = environ.get("MDB_DB_COLLECTION")
 
 uri = "mongodb+srv://"+MDB_USER+":"+MDB_PW+"@"+MDB+".uflbdbq.mongodb.net/?retryWrites=true&w=majority"
 
 conn = MongoClient(uri, server_api=ServerApi('1'))
-db = conn["pymongo"]
+db = conn[MDB_DB_INSTANCE]
 # Choose the collection to insert the document into
-collection = db["users"]
+collection = db[MDB_DB_COLLECTION]
